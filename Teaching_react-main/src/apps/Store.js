990d4cbf8d0ddd9cb72c0ad6cc63a2a46938e613
aayush12-bb <aayushsusling@gmail.com>
2026-08-08 20:59:50 +0545

    Initial commit

---
+++ Teaching_react-main/src/apps/Store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './Reducers/cartSlice';
+
+export const store = configureStore({
+  reducer: {
+
+    cart: cartReducer,
+
+  },
+});
+
